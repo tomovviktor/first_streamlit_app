aa9db0d11705bfaef42ae6ecb4b802aea152dc97
tomovviktor <133736995+tomovviktor@users.noreply.github.com>
2023-05-16 17:50:58 +0300

    Update streamlit_app.py
    Change all but the title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('this is a crash course!')
-streamlit.text('test text to serve the new purpose!')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
