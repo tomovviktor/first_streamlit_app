e4c3addbd0a6540f6edf95af3b7c3c3a7b280605
tomovviktor <133736995+tomovviktor@users.noreply.github.com>
2023-05-16 18:42:01 +0300

    Update streamlit_app.py
    delete all
    add parser for csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
-import streamlit
+import pandas
 
-streamlit.title('My Parents New Healthy Dinner')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg: 🥣 🥗 🐔 🥑🍞')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
