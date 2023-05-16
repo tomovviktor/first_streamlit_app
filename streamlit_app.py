414c38297e76b9706587093e82974c3ec401a108
tomovviktor <133736995+tomovviktor@users.noreply.github.com>
2023-05-16 18:49:32 +0300

    Update streamlit_app.py
    fix issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg: 🥣 🥗 🐔 🥑🍞')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.dataframe(my_fruit_list)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
