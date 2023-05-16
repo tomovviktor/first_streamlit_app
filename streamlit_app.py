f0180f847edf8db5d4d85e7d6e43670273d3344f
tomovviktor <133736995+tomovviktor@users.noreply.github.com>
2023-05-16 18:44:12 +0300

    Update streamlit_app.py
    combine all from history and csv reader

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,15 @@
+import streamlit
 import pandas
+
+streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg: 🥣 🥗 🐔 🥑🍞')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
