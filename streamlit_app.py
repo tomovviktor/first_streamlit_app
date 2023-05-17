ebaa460bd7a4bd68100314a48e0c99bb2f04556d
tomovviktor <133736995+tomovviktor@users.noreply.github.com>
2023-05-17 14:35:11 +0300

    Update streamlit_app.py
    add new textbox for inserting new fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,9 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
+
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
+streamlit.write('The user entered ', add_my_fruit)
