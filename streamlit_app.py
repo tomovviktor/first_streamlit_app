11e0ae7bdc74b4149083875f4c7a3c406421289e
tomovviktor <133736995+tomovviktor@users.noreply.github.com>
2023-05-17 16:50:35 +0300

    Update streamlit_app.py
    fix issue with line start

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,6 +71,6 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfr
 if streamlit.button('Add a fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
-streamlit.write('Thanks for adding ', back_from_function)
+    streamlit.write('Thanks for adding ', back_from_function)
 
 
